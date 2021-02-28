0316b7883ca53e4675bbb7085e7f1d31db10b8fd
frandy-yulia <frandy.yulia@gmail.com>
2021-03-01 00:56:06 +0700

    create view library end style library

--- app/main.js
+++ app/main.js
@@ -1,3 +1,24 @@
-define(function (require) {
-    console.log('ok');
+define(['view'],function (View) {
+
+    const Main = function(){
+
+
+        View.style({
+            main: {
+                display: 'flex',
+                flexDirection: 'row'
+            }
+        })
+
+        return (
+            View.createElement('div',{className: 'main'},[
+                View.createElement('div',null,'halo'),
+                View.createElement('div',null,'halo2'),
+                
+            ])
+        );
+    }
+
+    var app = document.getElementById('app');
+    app.appendChild(Main());
 });
